af46eb5b0280f4434409e4b26d2e6190b50eb1ed
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-09-06 14:45:55 +0530

    add to gsap properties

--- src/app/(basic)/to/page.tsx
+++ src/app/(basic)/to/page.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import gsap from "gsap";
 
-const page = () => {
-  return (
-    <div>
-      <h1>to</h1>
-    </div>
-  )
+interface TweenVars {
+  yoyo: boolean;
+  x: number;
+  duration: number;
+  repeat: number;
+  backgroundColor?: string;
+  borderRadius?: number;
 }
 
-export default page
+const Page = () => {
+  useEffect(() => {
+    // box1
+    const tweenVars: TweenVars = {
+      x: 200,
+      duration: 3,
+      repeat: -1,
+      yoyo: true,
+    };
+    gsap.to("#box1", tweenVars);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold capitalize my-3">
+        gsap properties to
+      </h1>
+
+      <div className="px-4 mb-10">
+        <p className="text-xl whitespace-nowrap">
+          {" "}
+          <span className="font-bold ">to</span> : is use for animate the
+          elements from initial to final
+        </p>
+      </div>
+
+      <div
+        id="box1"
+        className="bg-orange w-[100px] h-[100px] rounded-[12px]"
+        style={{ backgroundColor: "orange", borderRadius: 12 }}
+      ></div>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+"use client"
+import React, { useEffect } from "react";
 import Link from "next/link";
+import gsap from "gsap";
+
 const Home = () => {
+  useEffect(()=>{
+    gsap.to('Link',{
+       y:20,
+      opacity:1,
+      stagger:1,
+    })
+
+  },[])
+  
   return (
     <div className="p-2 ">
       <h1 className="text-2xl md:text-3xl font-bold  text-center mt-3">
